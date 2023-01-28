838d5f494baa7078e7c742359c5ac332b5e69729
redecoinproject <100836401+redecoinproject@users.noreply.github.com>
2023-01-28 07:53:46 +0100

    Update node.sh

--- node.sh
+++ node.sh
@@ -4,7 +4,7 @@ clear
 echo "node rede start"
 echo "cmd start"
 echo "curl https://raw.githubusercontent.com/redecoinproject/redecoin/master/node.sh | bash"
-sleep 1
+sleep 2
 wget https://raw.githubusercontent.com/redecoinproject/redecoin/master/genesis/genesis.json
 wget https://github.com/redecoinproject/redecoin/releases/download/v1.0.1/geth
 chmod 755 geth
