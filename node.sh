f984259f9dd9b9b07bd2a53f6b721866197d344d
redecoinproject <100836401+redecoinproject@users.noreply.github.com>
2023-01-28 07:56:26 +0100

    Update node.sh

--- node.sh
+++ node.sh
@@ -8,5 +8,6 @@ sleep 2
 wget https://raw.githubusercontent.com/redecoinproject/redecoin/master/genesis/genesis.json
 wget https://github.com/redecoinproject/redecoin/releases/download/v1.0.1/geth
 chmod 755 geth
+sleep 2
 ./geth init genesis.json
 ./geth --syncmode full console
