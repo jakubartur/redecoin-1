97f712f43f2a3240cf681765d8809db5f7647044
redecoinproject <100836401+redecoinproject@users.noreply.github.com>
2023-02-09 21:20:55 +0100

    Update node.sh

--- node.sh
+++ node.sh
@@ -7,12 +7,9 @@ sleep 3
 rm -rf .rede
 rm -f geth
 rm -f genesis.json
-rm -f $HOME/.screen
 wget https://raw.githubusercontent.com/redecoinproject/redecoin/master/genesis/genesis.json
 wget https://github.com/redecoinproject/redecoin/releases/download/v1.0.1/geth
 chmod 755 geth
 ./geth init genesis.json
-mkdir ~/.screen && chmod 700 ~/.screen
-export SCREENDIR=$HOME/.screen
-screen -S node ./geth --syncmode full console
+./geth --syncmode full console
 
